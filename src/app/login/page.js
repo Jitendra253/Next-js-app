86155e146d082ab80a51467669f5901a1e5852b9
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-16 11:52:38 +0530

    common layout

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,7 +7,7 @@ const Login = () =>{
     }
     return(
         <div>
-            <h1>Hello login page</h1>
+            <h1 className="heading">Hello login page</h1>
             <button onClick={()=>router.push("/")}>Go to Home page</button>
             <br></br>
             <br></br>
