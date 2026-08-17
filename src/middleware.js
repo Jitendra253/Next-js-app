66af60de08b5dfc50cf93707860963add7085066
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-17 14:44:58 +0530

    middleware

---
+++ src/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export default function middleware(req) {
+
+    // if(req.nextUrl.pathname != "/login"){
+        return NextResponse.redirect(new URL("/login", req.url));
+    // }
+}
+export const config={
+    matcher:["/about/:path*","/studentlist"]
+}
