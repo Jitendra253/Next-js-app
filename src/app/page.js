124aefe96996a0fee14866380bfaf70f59097e10
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-18 09:52:04 +0530

    contitional-style

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
-import custom from "./custom.module.css"
-import other from "./other.module.css"
-import outside from '@/style/outside.module.css'
+ 
+"use client"
+import { useState } from "react";
+import style from "./style.module.css";
+
+
 export default function Home(){
+  const [color,setColor] = useState('red')
+  const {red}= style;
   return(
     <div>
       <main>
-        <h1 className={custom.main}>Style and CSS with Next js</h1>
-        <h2 className={other.main}>Style and CSS with Next js</h2>
-        <h3 className={outside.main}>outside Style and css with next js</h3>
+        <h1 className={color=="red"?style.red:style.green}>Style and CSS with Next js</h1>
+        <h2 style={{backgroundColor:color==="red"?'red':'green'}}>Heading 2 </h2>
+        <h3 id={style.orange}>Heading 3 </h3>
+
+        <h4 className={red}>Dummy Text 1</h4>
+        <h4 className={red}>Dummy Text 2</h4>
+        <h4 className={red}>Dummy Text 3</h4>
+        <h4 className={red}>Dummy Text 4</h4>
+        <button onClick={()=>setColor("green")} >Update color</button>
       </main>
     </div>
   )
