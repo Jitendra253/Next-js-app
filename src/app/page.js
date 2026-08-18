fdc7de45de4a4a392239b675168a6c3813eb2e12
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-18 09:23:34 +0530

    css module styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+import custom from "./custom.module.css"
+import other from "./other.module.css"
+import outside from '@/style/outside.module.css'
 export default function Home(){
   return(
     <div>
       <main>
-        <h1 style={{backgroundColor:"yellow"}}>Style and CSS with Next js</h1>
-        <h2 className="main">Heading 2 in main page</h2>
-        <h3 className="main">Heading 3 in main </h3>
-        <p className="main">Some Paragraph</p>
+        <h1 className={custom.main}>Style and CSS with Next js</h1>
+        <h2 className={other.main}>Style and CSS with Next js</h2>
+        <h3 className={outside.main}>outside Style and css with next js</h3>
       </main>
     </div>
   )
