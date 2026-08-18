0952e42a8f933a611b8dd7242521fab3feeb3b59
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-18 15:59:07 +0530

    font optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fira+Code:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+      </head>
       <body>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,16 @@
-import Image from "next/image";
-import profile from "../../public/globe.svg";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight:'100',
+  subsets:['latin'],
+  display:'swap'
+})
 
 export default function Home() {
   return (
     <div>
-      <h1>Image Optimization in Next</h1>
-{/* 
-      <Image
-        src={profile} 
-        width={500}
-        height={500}
-        alt="example image"
-      />
-      <img src={profile.src}/> */}
-
-      <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr7Q2-iOUXeta4efD3d8FAPLTOfSN1lKRhOGJInoa6mA&s=10"
-       height={800} 
-       width={800} alt="sample image" />
+       <h1>Font Optimization</h1>
+       {/* <h1 style={{fontFamily:"roboto",fontWeight:"100"}}>Font Optimization</h1> */}
+       <h1 className={roboto.className}>Font withnext js font feature</h1>
     </div>
   );
 }
