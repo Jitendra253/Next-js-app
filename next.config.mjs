c43aec44429c06e70fcf43094acc47d4bd637fad
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-18 12:29:11 +0530

    image-optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,22 @@
- 
-"use client"
-import { useState } from "react";
-import style from "./style.module.css";
+import Image from "next/image";
+import profile from "../../public/globe.svg";
 
-
-export default function Home(){
-  const [color,setColor] = useState('red')
-  const {red}= style;
-  return(
+export default function Home() {
+  return (
     <div>
-      <main>
-        <h1 className={color=="red"?style.red:style.green}>Style and CSS with Next js</h1>
-        <h2 style={{backgroundColor:color==="red"?'red':'green'}}>Heading 2 </h2>
-        <h3 id={style.orange}>Heading 3 </h3>
+      <h1>Image Optimization in Next</h1>
+{/* 
+      <Image
+        src={profile} 
+        width={500}
+        height={500}
+        alt="example image"
+      />
+      <img src={profile.src}/> */}
 
-        <h4 className={red}>Dummy Text 1</h4>
-        <h4 className={red}>Dummy Text 2</h4>
-        <h4 className={red}>Dummy Text 3</h4>
-        <h4 className={red}>Dummy Text 4</h4>
-        <button onClick={()=>setColor("green")} >Update color</button>
-      </main>
+      <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr7Q2-iOUXeta4efD3d8FAPLTOfSN1lKRhOGJInoa6mA&s=10"
+       height={800} 
+       width={800} alt="sample image" />
     </div>
-  )
+  );
 }
