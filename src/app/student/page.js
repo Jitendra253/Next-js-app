18b19e319d601ded4fe3bcc3670801f16f7d1c88
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-19 14:52:25 +0530

    dynamic meta data

---
+++ src/app/student/page.js
@@ -0,0 +1,14 @@
+export default function Student(){
+    return(
+        <div>
+            <h1>Student Page</h1>
+        </div>
+    )
+}
+
+export function generateMetadata(){
+    return{
+        title:"student Page title",
+        description:"student page description"
+    }
+}
