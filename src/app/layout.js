36d100fe8c8f080c075fe9e3fc74eb4ab3ec4cfe
Jitendra Mahali <137375251+edusysJitendra@users.noreply.github.com>
2026-08-19 16:43:58 +0530

    staticassets

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fira+Code:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+        <link rel='stylesheet' href="/style.css"/>
+        <script src="/common.js" /> 
       </head>
       <body>{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
-import { Roboto } from "next/font/google";
-const roboto = Roboto({
-  weight:'100',
-  subsets:['latin'],
-  display:'swap'
-})
-
+ 
 export default function Home() {
   return (
     <div>
-       <h1>Font Optimization</h1>
-       {/* <h1 style={{fontFamily:"roboto",fontWeight:"100"}}>Font Optimization</h1> */}
-       <h1 className={roboto.className}>Font withnext js font feature</h1>
+       <h1>Static files</h1>
     </div>
   );
 }
